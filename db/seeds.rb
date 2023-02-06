7e5f70f4b36841958f8ae2a88991e8c29a88611c
rutger schoone <rcschoone@gmail.com>
2023-02-06 11:53:38 +0100

    made controllers, views, model and migrated db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Paris apartment',
+  address: '47 blvd tour maubourg',
+  description: 'A small place in a great area',
+  price_per_night: 50,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Light & Spacious Garden houde heemskerk',
+  address: 'terschelling 7',
+  description: 'A lovely summer feel for this spacious garden house',
+  price_per_night: 100,
+  number_of_guests: 7
+)
+Flat.create!(
+  name: 'villa in mauritius',
+  address: 'tamarin',
+  description: 'A lovely villa with a pool and a great view in west mauritius',
+  price_per_night: 150,
+  number_of_guests: 4
+)
